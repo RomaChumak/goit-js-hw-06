0061b71259679727e8014fcceb06dca5ac0ffb92
RomaChumak <romachumak355@gmail.com>
2023-07-05 17:49:45 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 const listEl = document.querySelector('#categories');
 const itemElement = listEl.children;
 console.log("Number of categories:", itemElement.length);
-itemElement.forEach(function (item) {
+[...itemElement].forEach(function (item) {
      console.log("Category:", item.children[0].textContent)
     console.log("Elements:", item.children[1].children.length)
 });
